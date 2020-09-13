f1bb1cc2ca8bd8149d6cf23e11eef929c8c3d177
Rj-batista <r.batista@outlook.fr>
2020-09-13 18:49:41 +0200

    Premier version avec lecture et creation tableau

--- test.py
+++ test.py
@@ -1,32 +1,39 @@
-import numpy as np  
-import pandas as pd
+def lecture(name):
+    flag_count=0
+    flag_shape=0 
+    with open(name, "r") as pdb_file: 
+        for line in pdb_file:
+            if line.startswith("ATOM"): 
+                flag_shape+=1   
+    p = np.full((flag_shape,9),np.nan) 
+    table = pd.DataFrame(p)
+    table.columns=["AA","position_AA","atome","position_atome","chaine","coordx","coordy","coordz","occupancy"]    
+    with open(name, "r") as pdb_file: 
+        for line in pdb_file:
+            if line.startswith("ATOM"): 
+                AA_name = line[17:20].strip() 
+                AA_position = line[22:26].strip()
+                atome_name = line[12:16].strip() 
+                atome_position = line[6:11].strip()
+                chaine = line[21]
+                x = float(line[30:38])
+                y = float(line[38:46])
+                z = float(line[46:54]) 
+                occupancy = line[54:60].strip()   
+                table.iloc[flag_count]=[AA_name,AA_position,atome_name,atome_position,chaine,x,y,z,occupancy] 
+                flag_count+=1
+        return(table) 
 
-flag_shape = 0 
-flag_count = 0
+def creation_table(table): 
+    table_souffre=pd.DataFrame((table.loc[(table['AA']=="CYS") | (table['AA'] =="MET")])) 
+    table_hydrophobic=pd.DataFrame((table.loc[(table['AA']=="ALA") | (table['AA'] =="VAL") | (table['AA'] =="LEU") | (table['AA'] =="ILE") | (table['AA'] =="MET") | (table['AA'] =="PHE") | (table['AA'] =="TRP") | (table['AA'] =="PRO") | (table['AA'] =="TYR")])) 
+    
+    
 
-with open("2gktH.pdb", "r") as pdb_file: 
-    for line in pdb_file:
-        if line.startswith("ATOM"): 
-            flag_shape += 1
-               
-p = np.full((flag_shape,9),np.nan) 
-table = pd.DataFrame(p)
-table.columns = ["AA","position_AA","atom","position_atom","chain","coord_x","coord_y","coord_z","occupancy"]    
 
-with open("2gktH.pdb", "r") as pdb_file: 
-    for line in pdb_file:
-        if line.startswith("ATOM"): 
-            AA_name = line[17:20].strip() 
-            AA_position = line[22:26].strip()
-            atom_name = line[12:16].strip() 
-            atom_position = line[6:11].strip()
-            chain = line[21]
-            x = float(line[30:38])
-            y = float(line[38:46])
-            z = float(line[46:54]) 
-            occupancy = line[54:60].strip()   
-            table.iloc[flag_count] = [AA_name,AA_position,atom_name,atom_position,chain,x,y,z,occupancy]
-            flag_count += 1
-            
-table['position_atom'] = table['position_atom'].replace(['0'],np.nan)
-print(table)
+
+if __name__ == "__main__": 
+    import numpy as np
+    import pandas as pd 
+    var_lecture= lecture("2gkt.pdb") 
+    var_creation_table = creation_table(var_lecture)
