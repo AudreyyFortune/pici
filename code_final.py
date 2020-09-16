66a9d05eefe34a840efa94e98a78dfea74bf4693
AudreyyFortune <audefortune@free.fr>
2020-09-16 04:04:57 +0200

    final

--- code_final.py
+++ code_final.py
@@ -75,7 +75,6 @@ def disulphide_bridges(table):
 	df_disulfure = pd.DataFrame((table.loc[(table['AA'] == "CYS") & (table['atom'] == "SG")]))
 	disulfure = np.array(df_disulfure)
 	for i in range(len(disulfure) -1):
-		j = j+i
 		for j in range(i+1, len(disulfure)):
 			distance = (np.sqrt(((disulfure[i,5] - disulfure[(j),5])**2) + ((disulfure[i,6] \
 						- disulfure[(j),6])**2) + ((disulfure[i,7] - disulfure[(j),7])**2)))
@@ -88,12 +87,14 @@ def disulphide_bridges(table):
 			liste_distance.append(distance) 
 			compteur += 1
 	array = np.array(liste_distance)
-	array = array.reshape((compteur,7))
+	array = array.reshape((compteur, 7))
 	array = pd.DataFrame(columns = ['position', 'residue', 'chain', 'position', 'residue', \
 				 'chain', 'distance'], data = array)
 	array['distance'] = array['distance'].astype(float)
+	array['distance'] = round(array['distance'], 2)
 	table_disulfure_cutoff = array[array["distance"] < cutoff]	
 	return(table_disulfure_cutoff)
+
 
 def ionic_interactions(table):
 	"""
@@ -140,9 +141,110 @@ def ionic_interactions(table):
 	array = pd.DataFrame(columns = ['position', 'residue', 'chain', 'position', 'residue', \
 				 'chain', 'distance'], data = array)
 	array['distance'] = array['distance'].astype(float)
+	array['distance'] = round(array['distance'], 2)
 	table_ionic_cutoff = array[array["distance"] < cutoff]
 	return(table_ionic_cutoff)
 		
+
+def mc_mc_interaction(table):
+	"""
+	Calcul les distances des interactions hydrogènes entre les chaines principales des résidus. 
+		
+		Parameters
+		----------
+			table : dataframe obtenu avec la fonction(lecture).
+				Contenant tous les atomes des acides aminés de fichier pdb.
+				
+		Returns
+		-------
+			dataframe
+				Contenant les interactions entre les chaines principales des résidus, 
+					définit par le cutoff (ici, 3.5 A).
+	"""
+	i, j, k, l = 0, 0, 0, 0
+	compteur = 0
+	cutoff = 3.5
+	liste_distance_no = []
+	liste_distance_ho = []
+	df_donor = pd.DataFrame((table.loc[(table['atom'] == "N")]))
+	df_donor = df_donor.iloc[::-1]
+	donor = np.array(df_donor)
+	df_acceptor = pd.DataFrame((table.loc[(table['atom'] == "O")]))
+	acceptor = np.array(df_acceptor)
+	df_donor_h = pd.DataFrame((table.loc[(table['atom'] == "H")]))
+	df_donor_h = df_donor_h.iloc[::-1]
+	donor_h = np.array(df_donor_h)
+	for i in range(len(donor))	:
+		for j in range(len(acceptor) -(i+2)):
+			distance_no = (np.sqrt(((donor[i,5] - acceptor[(j),5])**2) + ((donor[i,6] \
+						- acceptor[(j),6])**2) + ((donor[i,7] - acceptor[(j),7])**2)))
+			liste_distance_no.append(donor[i,1])
+			liste_distance_no.append(donor[i,4])
+			liste_distance_no.append(donor[i,0])
+			liste_distance_no.append(donor[i,2])
+			liste_distance_no.append(acceptor[j,1])
+			liste_distance_no.append(acceptor[j,4])
+			liste_distance_no.append(acceptor[j,0])	
+			liste_distance_no.append(acceptor[j,2])
+			liste_distance_no.append(distance_no)
+			compteur += 1
+	for k in range(len(donor_h)):
+		for l in range(len(acceptor) -(k+2)):
+			distance_ho = (np.sqrt(((donor_h[k,5] - acceptor[(l),5])**2) + ((donor_h[k,6] \
+						- acceptor[(l),6])**2) + ((donor_h[k,7] - acceptor[(l),7])**2)))
+			liste_distance_ho.append(distance_ho)
+	array = np.array(liste_distance_no)
+	array = array.reshape((compteur,9))
+	df = pd.DataFrame(columns = ['position', 'chain', 'residu', 'atom', 'position', \
+				 'chain', 'residu', 'atom', 'Dd-a'], data = array)
+	df['Dd-a'] = df['Dd-a'].astype(float)
+	df['Dd-a'] = round(df['Dd-a'], 2)
+	df_ho = pd.DataFrame(liste_distance_ho, columns = ['Dh-a'])
+	df_ho['Dh-a'] = round(df_ho['Dh-a'], 2)
+	df_no_ho = pd.concat([df,df_ho], axis = 1)
+	table_mc_mc_cutoff = df_no_ho[df_no_ho["Dd-a"] < cutoff]
+	return(table_mc_mc_cutoff)
+	
+
+def interaction_aromatic(table):
+	"""
+	"""
+	i = 0
+	j = 0
+	compteur = 0
+	cutoff_min = 4.5
+	cutoff_max = 7
+	liste_distance = []
+	df_aromatic = pd.DataFrame((table.loc[((table['AA'] == "TYR") | (table['AA'] == "PHE") \
+				| (table['AA'] == "TRP")) & ((table['atom'] == "CD1") | (table['atom'] == \
+				"CD2") | (table['atom'] == "CE1") | (table['atom'] == "CZ") | (table['atom'] \
+				 == "CE2") | (table['atom'] == "CG") | (table['atom'] == "NE1") | \
+				 (table['atom'] == "CZ2") | (table['atom'] == "CH2") | (table['atom'] == \
+				 "CZ3") | (table['atom'] == "CE3"))]))
+	aromatique = np.array(df_aromatic)
+	for i in range(len(aromatique) -1):
+		for j in range(i+1, len(aromatique)):
+			distance = (np.sqrt(((aromatique[i,5] - aromatique[(j),5])**2) + ((aromatique[i,6] \
+						- aromatique[(j),6])**2) + ((aromatique[i,7] - aromatique[(j),7])**2)))
+			liste_distance.append(aromatique[i,1])
+			liste_distance.append(aromatique[i,0])
+			liste_distance.append(aromatique[i,4])
+			liste_distance.append(aromatique[j,1])
+			liste_distance.append(aromatique[j,0])
+			liste_distance.append(aromatique[j,4])			
+			#liste_distance.append(distance) 
+			#compteur += 1
+	#array = np.array(liste_distance)
+	#array = array.reshape((compteur, 7))
+	#array = pd.DataFrame(columns = ['position', 'residue', 'chain', 'position', 'residue', \
+	#			 'chain', 'distance'], data = array)
+	#array['distance'] = array['distance'].astype(float)
+	#array['distance'] = round(array['distance'], 2)
+	#total = array['distance'].sum()
+	#tt = total /26
+	#print(tt)
+	#table_aromatic_cutoff = array[ (array["distance"] > cutoff_min) & (array["distance"] < cutoff_max)]	
+	return(liste_distance)
 
 
 
@@ -172,4 +274,10 @@ if __name__ == "__main__":
     		print("---------------------------------------------------------------------")
     		ionic = ionic_interactions(var_lecture)
     		print("""Intraprotein Ionic Interactions \n Ionic Interactions within 6 Angstroms \n """, ionic)
-    		print()
+    		print("---------------------------------------------------------------------")
+    		mc_mc = mc_mc_interaction(var_lecture)
+    		print("Intraprotein Main Chain-Main Chain Hydrogen Bonds \n")
+    		print(" 	DONOR 			ACCEPTOR 		PARAMETERS \n" ,mc_mc)
+    		print("---------------------------------------------------------------------")
+    		maybe = interaction_aromatic(var_lecture)
+    		#print(maybe)
